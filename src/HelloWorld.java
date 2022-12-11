6e082edc3acf3036a0d48e1708bbe9c6ffb8e2c1
xmy <xmy2000@sjtu.edu.cn>
2022-12-11 16:32:31 +0800

    xmychange

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ package src;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Github!");
+        System.out.println("Hello Github22222222!");
     }
 }
