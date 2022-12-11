14f14c8eccc94a6616d284e7bf2231c19244918b
xmy <xmy2000@sjtu.edu.cn>
2022-12-11 16:19:16 +0800

    Revert "test22222"
    This reverts commit fe03d91a4bbf6699d9e860c060242eb8b1d52098.

--- src/Hello2.java
+++ src/Hello2.java
@@ -2,6 +2,6 @@ package src;
 
 public class Hello2 {
     public static void main(String[] args) {
-        System.out.println("Hello Git2222222!");
+        System.out.println("Hello Git!");
     }
 }
